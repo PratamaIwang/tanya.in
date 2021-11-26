a18eb42fbb7390c2919bdd881b8d65a3910dad81
Pratama-E <endrapratama@student.ub.ac.id>
2021-11-26 23:39:29 +0700

    ga tau udah mentok

---
+++ app/src/main/java/com/example/tanyain/Browseimage.kt
@@ -0,0 +1,11 @@
+package com.example.tanyain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Browseimage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browseimage)
+    }
+}
