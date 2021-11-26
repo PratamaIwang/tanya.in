02fc14f8f44814e3d98567f32918f6c9ec70290c
PratamaIwang <pratamaiwan16@gmail.com>
2021-11-26 09:48:01 +0700

    BackEnd Regis and Login

--- app/src/main/java/com/example/tanyain/RegisterActivity.kt
+++ app/src/main/java/com/example/tanyain/RegisterActivity.kt
@@ -1,58 +1,95 @@
 package com.example.tanyain
 
-import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.core.view.isVisible
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+
 
 class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var inpRegUsername: EditText
-    private lateinit var inpRegPassword: EditText
+    private lateinit var inpEmailReg: EditText
+    private lateinit var inpPasswordReg: EditText
     private lateinit var inpConfPassword: EditText
     private lateinit var register: TextView
-    private lateinit var BtnRegister: Button
+    private lateinit var btnRegister: Button
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val dbHelper = DBHelper(this);
-
-        inpRegUsername = findViewById(R.id.inpUsernameReg)
-        inpRegPassword = findViewById(R.id.inpPasswordReg)
+        inpEmailReg = findViewById(R.id.inpEmailReg)
+        inpPasswordReg = findViewById(R.id.inpPasswordReg)
         inpConfPassword = findViewById(R.id.inpConfPassword)
-        BtnRegister = findViewById(R.id.btnRegister)
-
         register = findViewById(R.id.tvRegister)
+        btnRegister = findViewById(R.id.btnRegister)
+        progressBar = findViewById(R.id.pb_register)
 
-        register.setOnClickListener{
-            startActivity(Intent(this, LoginActivity::class.java))
-        }
+        mAuth = Firebase.auth
+        database = FirebaseDatabase.getInstance().getReference("tanyain")
 
-        BtnRegister.setOnClickListener{
-            var username :String = inpRegUsername.getText().toString().trim()
-            var password :String = inpRegPassword.getText().toString().trim()
-            var confPassword :String = inpConfPassword.getText().toString().trim()
 
-            var values :ContentValues = ContentValues()
+        btnRegister.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            var email = inpEmailReg.text.toString().trim()
+            var password = inpPasswordReg.text.toString().trim()
+            var passwordconf = inpConfPassword.text.toString().trim()
 
-            if (password.equals(confPassword)){
-                values.put(DBHelper.row_username, username)
-                values.put(DBHelper.row_password, password)
-                dbHelper.inserData(values)
-
-                Toast.makeText(this,"Register Successfull", Toast.LENGTH_SHORT).show()
-                finish()
-            }else if (password.equals("")|| username.equals("")){
-                Toast.makeText(this,"Username or Password cant be empty", Toast.LENGTH_SHORT).show()
-            }else {
-                Toast.makeText(this,"Password not match", Toast.LENGTH_SHORT).show()
+            if (email.isEmpty()) {
+                Toast.makeText(this, "Email is required!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+            if (password.isEmpty()) {
+                Toast.makeText(this, "Password is required!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (passwordconf.isEmpty()) {
+                Toast.makeText(this, "Password is required!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (password.length < 8) {
+                Toast.makeText(
+                    this,
+                    "Min password length should be 8 characters!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val uid: String = FirebaseAuth.getInstance().currentUser!!.uid
+                        val user = User(email, password, null, null, null, null, null, null)
+                        database.child("users").child(uid).setValue(user)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    inpEmailReg.setText("")
+                                    inpPasswordReg.setText("")
+                                    inpConfPassword.setText("")
+                                    progressBar.visibility = View.GONE
+                                    Toast.makeText(this, "Login Successfull!", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    } else {
+                        Toast.makeText(this, "Login failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
