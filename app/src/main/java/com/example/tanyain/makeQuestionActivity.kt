1be856095f9e0e824dd4b54260b47413e0cf25df
Kautsarratu <64664923+Kautsarratu@users.noreply.github.com>
2021-11-26 15:50:26 +0700

    nambah card

---
+++ app/src/main/java/com/example/tanyain/makeQuestionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tanyain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class makeQuestionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_question)
+    }
+}
