6a1be49fc57f80131accbef0b0a4502b47becfe9
Kautsarratu <64664923+Kautsarratu@users.noreply.github.com>
2021-11-27 18:10:06 +0700

    adapter

---
+++ app/src/main/java/com/example/tanyain/QuestionsModel.kt
@@ -0,0 +1,10 @@
+package com.example.tanyain
+
+import java.util.*
+
+class QuestionsModel(
+    val nama: String,
+    val tanggal: String,
+    val kategori: String,
+    val pertanyaan: String,
+)
